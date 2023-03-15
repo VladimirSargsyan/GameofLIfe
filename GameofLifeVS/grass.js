22f298b211199f884875b90ebd1749cfb781ea5f
NarekFrangyan <narek.frangyan.g@tumo.org>
2023-03-15 18:35:58 +0400

    inhertiance

---
+++ GameofLifeVS/grass.js
@@ -0,0 +1,27 @@
+class Grass extends LivingCreature {
+    constructor(x,y){
+           super(x,y)
+            this.multiply = 2
+          
+        
+    }
+
+   
+    mul(){
+        this.multiply++
+        var emptyCell = this.chooseCell(0)
+        var newCell = random(emptyCell)
+
+            if(this.multiply >= 8 && newCell){
+                  var newX  = newCell[0]
+                  var newY = newCell[1]
+
+                  matrix[newY][newX] = 1
+
+                  var gr  = new Grass(newX,newY)
+                  grassArr.push(gr)
+                  this.multiply = 2
+            }
+
+    }
+}
